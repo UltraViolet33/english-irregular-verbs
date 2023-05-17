bf97bb4d07e66db8259bc91360e54cec33e2f636
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-05-17 16:52:52 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php require_once './inc/header.php' ?>
+<div class="container my-5">
+    <div class="row">
+        <div class="col">
+            <h1 class="text-center">Révisions</h1>
+        </div>
+    </div>
+</div>
+<?php require_once './inc/footer.php' ?>
